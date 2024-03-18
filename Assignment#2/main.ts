2310f9a9aa304b5dd7087886f4815054a9a9c5ec
student-Kiranrao <kr3119842@gmail.com>
2024-03-19 01:42:24 +0500

    Add new titlecase

--- Assignment#2/main.ts
+++ Assignment#2/main.ts
@@ -1,10 +1,15 @@
-//storing a person name in a variable
+//Name Cases: Store a person’s name in a variable, and
+//  then print that person’s name in lowercase, uppercase, and titlecase.
+
+// storing a person name in a variable
+
 
 let personName :string = "Nasim Akhter";
 //printing a person name in a lowercase
  console.log("Lowercase:",personName.toLowerCase())
 
-
+// printing a person name in a UpperCase
  console.log("Uppercase:",personName.toUpperCase());
 
-
+// printing a person name in a TitleCase
+console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase);
